1c0228653f635e771ec01616fbfc8f4089cd2721
daizheng6 <daizheng@jd.com>
2016-07-22 15:45:05 +0800

    提交

---
+++ app/actions/AddCharacterActions.js
@@ -0,0 +1,28 @@
+import alt from '../alt';
+
+class AddCharacterActions {
+    constructor(){
+        this.generateActions(
+            'addCharacterSuccess',
+            'addCharacterFail',
+            'updateName',
+            'updateGender',
+            'invalidName',
+            'invalidGender'
+        );
+    }
+
+    addCharacter(name, gender){
+        $.ajax({
+            type: 'POST',
+            url: '/api/characters',
+            data:{name: name, gender: gender}
+        }).done((data) => {
+            this.actions.addCharacterSuccess(data.message);
+        }).fail((jqXhr) => {
+            this.actions.addCharacterFail(jqXhr);
+        });
+    }
+}
+
+export default alt.createActions(AddCharacterActions);
